2f5ca31f7c7698aa0493a3a8fa134135c567c9ac
joelKamps <joel.kamps1@gmail.com>
2017-11-10 08:45:08 +0100

    clean project

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,5 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-/**
- * Created by joel_thinkpad on 10.11.2017.
- */
 public class Main {
 
     public static void main (String[] args) {
